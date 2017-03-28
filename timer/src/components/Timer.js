7999fed2cbc6cc10d6ba7ebcdfa87e693663286d
heq <henri.levalampi@gmail.com>
2017-03-28 18:10:19 +0300

    Timer project with some initial testing

---
+++ timer/src/components/Timer.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var Clock = require('Clock');
+
+var Timer = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>T</h1>
+            </div>
+        );
+    }
+});
+
+module.exports = Timer;
---
+++ timer/webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'script!jquery/dist/jquery.min.js',
+        'script!foundation-sites/dist/js/foundation.min.js',
+        './src/app.js',
+        './src/index.html'
+    ],
+    // to have variables available on the entry files(?)
+    externals: {
+        jquery: 'jQuery',
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: __dirname + '/dist'
+    },
+    resolve: {
+        root: __dirname,
+        alias: {
+            Main: 'src/components/Main.js',
+            Navigation: 'src/components/Navigation.js',
+            Timer: 'src/components/Timer.js',
+            Countdown: 'src/components/Countdown.js',
+            Clock: 'src/components/Clock.js',
+            applicationStyles: 'src/styles/app.scss',
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/
+            },
+            {
+                test: /index\.html/,
+                loader: 'file-loader?name=[name].[ext]'
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+};
